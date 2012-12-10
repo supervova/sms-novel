c686db7061b50fe37560c4eb1294a3c4eb0c82de
supervova <nickysheen@yandex.ru>
2012-12-10 21:16:01 +0400

    <del>10</del>

--- a/js/scripts.js
+++ a/js/scripts.js
@@ -119,11 +119,57 @@ function tooltipSmiley() {
 
 // A responsive images approach including Retina image replacement — Picturefill - https://github.com/scottjehl/picturefill.
 
+// Text rotator
+(function($) {
+    $.fn.extend({
+        //plugin name - rotaterator
+        rotaterator: function(options) {
+
+            var defaults = {
+                fadeSpeed: 600,
+                pauseSpeed: 100,
+                child: null
+            };
+
+            var options = $.extend(defaults, options);
+
+            return this.each(function() {
+                var o = options;
+                var obj = $(this);
+                var items = $(obj.children(), obj);
+                items.each(function() {
+                    $(this).hide();
+                });
+                if(!o.child) {
+                    var next = $(obj).children(':first');
+                } else {
+                    var next = o.child;
+                }
+                $(next).fadeIn(o.fadeSpeed, function() {
+                    $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+                        var next = $(this).next();
+                        if(next.length === 0) {
+                            next = $(obj).children(':first');
+                        }
+                        $(obj).rotaterator({
+                            child: next,
+                            fadeSpeed: o.fadeSpeed,
+                            pauseSpeed: o.pauseSpeed
+                        });
+                    });
+                });
+            });
+        }
+    });
+})(jQuery);
+
+
 $(document).ready(function(){
     $('iframe[src^="http://player.vimeo.com"], iframe[src^="http://www.youtube.com"], iframe[src*="dailymotion.com"], object:not([class*="not-video"]):not(:has(embed)), embed:not([class*="not-video"])').wrap('<figure class="video" />');
     $('ol, ul').prev('p').css('margin-bottom', '0'); //lists captions
     dropDowns();
     tooltipSmiley();
+    $('.box-testimonials').rotaterator({fadeSpeed:1200, pauseSpeed:8000});
 });
 
 function noError() {
