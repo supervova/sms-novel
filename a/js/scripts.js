858d05212105b50eb336a0297fcd493452d567a9
supervova <nickysheen@yandex.ru>
2013-01-15 19:43:57 +0400

    tumblrLike uped

--- a/js/scripts.js
+++ a/js/scripts.js
@@ -158,11 +158,11 @@ function tumblrLike() {
 }
 
 $(document).ready(function(){
+    tumblrLike();
     $('iframe[src^="http://player.vimeo.com"], iframe[src^="http://www.youtube.com"], iframe[src*="dailymotion.com"], object:not([class*="not-video"]):not(:has(embed)), embed:not([class*="not-video"])').wrap('<figure class="video" />');
     $('ol, ul').prev('p').css('margin-bottom', '0'); //lists captions
     dropDowns();
     tooltipSmiley();
-    tumblrLike();
     $('.box-testimonials').rotaterator({fadeSpeed:1200, pauseSpeed:8000});
     $('<div class="box-cta-hr"></div>').insertBefore('.box-cta .h-secondary');
 
