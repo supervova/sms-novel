a31d90a152cb46f967984644a6d29e8fd605359d
supervova <nickysheen@yandex.ru>
2013-03-16 19:41:52 +0400

    slideOut()

--- a/js/scripts.js
+++ a/js/scripts.js
@@ -222,6 +222,7 @@ $(document).ready(function(){
     dropDowns();
     tooltipSmiley();
     tumblrLike();
+    slideOut();
     $('.box-testimonials').rotaterator({fadeSpeed:1200, pauseSpeed:8000});
     $('<div class="box-cta-hr"></div>').insertBefore('.box-cta .h-secondary');
     $('.btn-back').click(function(){ parent.history.back(); return false;});
