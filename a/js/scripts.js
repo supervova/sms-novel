8f5a80f9d641c8a7edd086217d14fa7dc880cb69
supervova <nickysheen@yandex.ru>
2013-08-27 11:08:02 +0200

    jquery responsive video

--- a/js/scripts.js
+++ a/js/scripts.js
@@ -187,6 +187,41 @@ function tumblrLike() {
     });
 }
 
+/*
+|-------------------------------------------------------------------------------
+| RESPONSIVE VIDEO
+|-------------------------------------------------------------------------------
+*/
+function responsiveVideo() {
+
+    var $allVideos = $('iframe[src^="http://player.vimeo.com"], iframe[src^="http://www.youtube.com"], iframe[src*="dailymotion.com"], iframe[src*="//instagram.com/"], iframe[src^="http://coub.com"], object, embed'),
+    $fluidEl = $('figure');
+
+    $allVideos.each(function() {
+
+      $(this)
+        // jQuery .data does not work on object/embed elements
+        .attr('data-aspectRatio', this.height / this.width)
+        .removeAttr('height')
+        .removeAttr('width');
+
+    });
+
+    $(window).resize(function() {
+
+      var newWidth = $fluidEl.width();
+      $allVideos.each(function() {
+
+        var $el = $(this);
+        $el
+            .width(newWidth)
+            .height(newWidth * $el.attr('data-aspectRatio'));
+
+      });
+
+    }).resize();
+
+}
 
 /*
 |-------------------------------------------------------------------------------
